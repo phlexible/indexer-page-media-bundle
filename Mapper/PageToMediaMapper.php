3c48b8a94dbebde9f6a652cf6331c7959d964414
Jens Schulze <jdschulze@brainbits.net>
2017-03-22 14:19:58 +0100

    Reformat PageToMediaMapper.php

--- Mapper/PageToMediaMapper.php
+++ Mapper/PageToMediaMapper.php
@@ -16,10 +16,10 @@ use Elastica\Filter\Term;
 use Elastica\Query;
 use Elastica\Result;
 use Phlexible\Bundle\ElasticaBundle\Elastica\Index;
+use Phlexible\Bundle\IndexerMediaBundle\Document\MediaDocument;
 use Phlexible\Bundle\IndexerMediaBundle\Indexer\MediaDocumentDescriptor;
 use Phlexible\Bundle\IndexerPageMediaBundle\Event\MapDocumentEvent;
 use Phlexible\Bundle\IndexerPageMediaBundle\IndexerPageMediaEvents;
-use Phlexible\Bundle\IndexerMediaBundle\Document\MediaDocument;
 use Phlexible\Bundle\MediaManagerBundle\Entity\FolderUsage;
 use Phlexible\Bundle\SiterootBundle\Entity\Siteroot;
 use Phlexible\Bundle\SiterootBundle\Model\SiterootManagerInterface;
@@ -31,7 +31,7 @@ use Phlexible\Component\Volume\VolumeManager;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 /**
- * Page to media mapper
+ * Page to media mapper.
  *
  * @author Phillip Look <pl@brainbits.net>
  */
@@ -49,6 +49,7 @@ class PageToMediaMapper
 
     /**
      * TODO: NEVER USED!!!
+     *
      * @var VolumeManager
      */
     private $volumeManager;
@@ -81,7 +82,7 @@ class PageToMediaMapper
     /**
      * @param Connection               $connection
      * @param EventDispatcherInterface $dispatcher
-     * @param VolumeManager            $volumeManager      NEVER USED!!!
+     * @param VolumeManager            $volumeManager NEVER USED!!!
      * @param TreeManager              $treeManager
      * @param FileUsageManager         $fileUsageManager
      * @param FolderUsageManager       $folderUsageManager
@@ -98,18 +99,18 @@ class PageToMediaMapper
         SiterootManagerInterface $siterootManager,
         Index $index
     ) {
-        $this->connection         = $connection;
-        $this->dispatcher         = $dispatcher;
-        $this->volumeManager      = $volumeManager; // NEVER USED!!!
-        $this->treeManager        = $treeManager;
-        $this->fileUsageManager   = $fileUsageManager;
+        $this->connection = $connection;
+        $this->dispatcher = $dispatcher;
+        $this->volumeManager = $volumeManager; // NEVER USED!!!
+        $this->treeManager = $treeManager;
+        $this->fileUsageManager = $fileUsageManager;
         $this->folderUsageManager = $folderUsageManager;
-        $this->siterootManager    = $siterootManager;
-        $this->index              = $index;
+        $this->siterootManager = $siterootManager;
+        $this->index = $index;
     }
 
     /**
-     * Add element information to document
+     * Add element information to document.
      *
      * @param MediaDocument           $mediaDocument
      * @param MediaDocumentDescriptor $descriptor
@@ -134,7 +135,7 @@ class PageToMediaMapper
             foreach ($results as $result) {
                 /* @var $result Result */
                 $siterootId = $result->getData()['siterootId'];
-                $siteroot   = $this->siterootManager->find($siterootId);
+                $siteroot = $this->siterootManager->find($siterootId);
 
                 if ($this->elementContainsFile($mediaDocument, $result, $siteroot)) {
                     $this->applyPageFields($mediaDocument, $result);
@@ -145,7 +146,7 @@ class PageToMediaMapper
 
     private function findPageDocuments($nodeId)
     {
-        $query  = new Query();
+        $query = new Query();
         $filter = new Term();
         $filter->setTerm('typeId', $nodeId);
         $query->setPostFilter($filter);
@@ -155,7 +156,7 @@ class PageToMediaMapper
     }
 
     /**
-     * @param MediaDocument $mediaDocument
+     * @param MediaDocument         $mediaDocument
      * @param ExtendedFileInterface $file
      *
      * @return array
@@ -188,7 +189,7 @@ class PageToMediaMapper
 
     /**
      * @param ExtendedFileInterface $file
-     * @param string[]      $parentFolderIds
+     * @param string[]              $parentFolderIds
      *
      * @return array
      */
@@ -219,22 +220,22 @@ class PageToMediaMapper
      */
     private function elementContainsFile(MediaDocument $mediaDocument, Result $pageDocument, Siteroot $siteroot)
     {
-        $data       = $pageDocument->getData();
+        $data = $pageDocument->getData();
         $siterootId = $data['siterootId'];
-        $language   = $data['language'];
-        $nodeId     = $data['nodeId'];
-        $typeId     = $data['typeId'];
+        $language = $data['language'];
+        $nodeId = $data['nodeId'];
+        $typeId = $data['typeId'];
 
-        $tree          = $this->treeManager->getBySiteRootId($siterootId);
-        $node          = $tree->get($nodeId);
+        $tree = $this->treeManager->getBySiteRootId($siterootId);
+        $node = $tree->get($nodeId);
         $onlineVersion = $tree->getPublishedVersion($node, $language);
 
         if (!$onlineVersion) {
             return false;
         }
 
-        $folderId    = $mediaDocument->get('folder_id');
-        $fileId      = $mediaDocument->get('file_id');
+        $folderId = $mediaDocument->get('folder_id');
+        $fileId = $mediaDocument->get('file_id');
         $fileVersion = $mediaDocument->get('file_version');
 
         $qb = $this->connection->createQueryBuilder();
@@ -262,7 +263,7 @@ class PageToMediaMapper
 
         $result = $this->connection->fetchAssoc($qb->getSQL());
 
-        return (bool) $result;
+        return (bool)$result;
     }
 
     /**
@@ -272,10 +273,10 @@ class PageToMediaMapper
     private function applyPageFields(MediaDocument $mediaDocument, Result $pageDocument)
     {
         $mapping = [
-            'typeIds'     => 'typeId',
-            'nodeIds'     => 'nodeId',
+            'typeIds' => 'typeId',
+            'nodeIds' => 'nodeId',
             'siterootIds' => 'siterootId',
-            'languages'   => 'language',
+            'languages' => 'language',
         ];
 
         $this->mergeFields($mediaDocument, $pageDocument, $mapping);
@@ -306,15 +307,14 @@ class PageToMediaMapper
     {
         $mediaValue = $mediaDocument->get($mediaField);
         if ($mediaValue) {
-            $mediaValue = (array) $mediaValue;
+            $mediaValue = (array)$mediaValue;
         } else {
             $mediaValue = [];
         }
-        $elementValue = (array) $pageDocument->getData()[$elementField];
+        $elementValue = (array)$pageDocument->getData()[$elementField];
 
         $newValue = array_unique(array_merge($mediaValue, $elementValue));
 
         $mediaDocument->set($mediaField, $newValue);
     }
 }
-
